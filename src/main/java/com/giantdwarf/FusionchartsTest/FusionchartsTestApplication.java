fc38fb2cdd24c7be49e18027a35db2961d210b34
seungin yang <rhfpdk92@naver.com>
2019-06-06 14:41:09 +0900

    프로젝트 생성

---
+++ src/main/java/com/giantdwarf/FusionchartsTest/FusionchartsTestApplication.java
@@ -0,0 +1,13 @@
+package com.giantdwarf.FusionchartsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusionchartsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusionchartsTestApplication.class, args);
+	}
+
+}
