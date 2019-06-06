828cd86e7656932652b5e7e1e43f30150429b102
seungin yang <rhfpdk92@naver.com>
2019-06-06 23:41:51 +0900

    적당한 막대바차트 찾음

---
+++ src/main/java/com/giantdwarf/FusionchartsTest/web/ChartController.java
@@ -0,0 +1,17 @@
+package com.giantdwarf.FusionchartsTest.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+public class ChartController {
+	
+	@GetMapping("/")
+    public String main() {
+        return "main";
+    }
+
+}
